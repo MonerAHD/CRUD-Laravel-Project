7895dea40ecb38d0f345df4ee2d3003d064f6673
Moner AHD <monerzk10@gmail.com>
2024-11-25 00:40:19 +0300

    Updated

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,4 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('posts.index');  
-
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
-Route::get('/posts/show', [PostController::class, 'show'])->name('posts.show');
-
-Route::get('/posts/edit', [PostController::class, 'edit'])->name('posts.edit');
-
-Route::post('/', [PostController::class, 'store'])->name('posts.store');
+Route::resource('posts', PostController::class);
